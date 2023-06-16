d8fb09bc4b144a6566ea356f115ef2e3880453c3
mjamal <mjamal@monetplus.cz>
2023-06-16 14:19:29 +0200

    Created Basic Structure

---
+++ src/test/java/mkj/e_commerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package mkj.e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
